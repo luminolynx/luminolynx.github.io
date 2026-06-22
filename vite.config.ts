c0d75205bf9d0e5446eea269a7f9c25803849db4
Adhima <anfaadhima@gmail.com>
2026-06-22 12:25:41 +0700

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: './tes-cat',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
